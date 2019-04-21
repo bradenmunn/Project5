08a8900988b8fe61fadab728188e6c57345c23f9
bradenmunn <bradenmunn@yahoo.com>
2019-04-20 21:58:36 -0500

    Initial commit

---
+++ src/GraphicalHammingDistance.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class GraphicalHammingDistance extends JFrame
+{
+	
+	@SuppressWarnings("unused")
+	private final class GraphicalCalculatorPanel extends JPanel
+	{
+		
+	}
+
+	/**
+	 * Main method to the program. Creates a new GraphicalCalculatorFrame object,
+	 * calling its constructor.
+	 *
+	 * @param args The program arguments.
+	 */
+	public static void main(String[] args) {
+		new GraphicalHammingDistance();
+	}
+
+}

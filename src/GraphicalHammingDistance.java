87cb97fd7c9e2e6cdbd62273dd24a9ea0dfafd51
bradenmunn <bradenmunn@yahoo.com>
2019-04-20 22:18:34 -0500

    Setup class structure of GraphicalHammingDistance.java

--- src/GraphicalHammingDistance.java
+++ src/GraphicalHammingDistance.java
@@ -1,10 +1,22 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+/**
+ * Class representing an interactive HammingDistance calculator.
+ *
+ * @author Braden Munn
+ * @version 2019-04-20
+ */
 @SuppressWarnings("serial")
 public class GraphicalHammingDistance extends JFrame
 {
+	private static final int FRAME_WIDTH = 700;
+	private static final int FRAME_HEIGHT = 800;
 	
+	/**
+	 * Interactive panel that the user clicks on to modify calculation of HammingDistance between stations. 
+	 * The panel has multiple Graphical elements to perform different tasks
+	 */
 	@SuppressWarnings("unused")
 	private final class GraphicalCalculatorPanel extends JPanel
 	{
